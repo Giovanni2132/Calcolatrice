064ed4c58f388969f0c68ddc26099a4230ce99f6
MicheleLetterese <m.letterese@studenti.unisa.it>
2023-09-28 15:23:09 +0200

    Inserito due funzioni: sottrazione e divisione

--- src/main/java/org/example/Calcoltarice.java
+++ src/main/java/org/example/Calcoltarice.java
@@ -14,4 +14,12 @@ public class Calcoltarice {
     public int moltiplicazione(int a,int b){
         return a*b;
     }
+
+    public int sottrazione(int a, int b){
+        return a-b;
+    }
+
+    public int divisione(int a, int b){
+        return a/b;
+    }
 }
